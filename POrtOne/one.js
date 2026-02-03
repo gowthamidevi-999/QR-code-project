9d046588011aae03c34766efe273a459a90e4aac
gowthamidevi <gowthamidevi53@gmail.com>
2026-02-03 22:30:13 +0530

    Remove all files from Github

--- POrtOne/one.js
+++
@@ -1,5 +0,0 @@
- const hamberger=document.querySelector(".ham");
-const navbar=document.querySelector(".navbar-menu .menubar");
- hamberger.addEventListener("click",()=>{
-navbar.classList.toggle("show");
-        })
