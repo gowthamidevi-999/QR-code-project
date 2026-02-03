e2724177849ef146352b2780643164641263ae1c
gowthamidevi <gowthamidevi53@gmail.com>
2026-02-03 19:18:02 +0530

    Initial commit - personl portfolio including QR code and Projects

---
+++ POrtOne/one.js
@@ -0,0 +1,5 @@
+ const hamberger=document.querySelector(".ham");
+const navbar=document.querySelector(".navbar-menu .menubar");
+ hamberger.addEventListener("click",()=>{
+navbar.classList.toggle("show");
+        })
